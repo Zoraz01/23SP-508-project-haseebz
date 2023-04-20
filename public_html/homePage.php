ea726e3733e1887b45b5b49e0ba6518fa7bd49fe
Zoraz Haseeb <Zorazhaseeb1@gmail.com>
2023-04-20 15:05:47 -0400

    Added navBar and HomePage

---
+++ public_html/homePage.php
@@ -0,0 +1,49 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Premier League Home Page</title>
+    <link rel="stylesheet" href="homePage.css">
+    <link href = https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css rel= "stylesheet">
+
+    <style>
+      .center-image {
+        display: flex;
+        justify-content: center;
+        align-items: center; 
+      }
+      .h1-welcome {
+        display: flex;
+        justify-content: center;
+        align-items: center; 
+        color: #38003c;
+      }
+      .button-container {
+        display: flex;
+        justify-content: center;
+        align-items: center; 
+        margin-top: 20px;
+      }
+      
+    </style>
+
+    <?php require_once('header.php'); ?>
+  </head>
+
+  
+  <body style = "background-color: #04f5ff;">
+    <?php require_once('navBar.php'); ?>
+    <!--<h1>Hello, world!</h1>-->
+    <div class = "center-image">
+      <img src="images/Premier_League-Logo.wine.png" height = "400" width = "600">
+    </div>
+    
+    <h1 class = "h1-welcome">The Home of Premier League Statistics</h1>
+    <div class="button-container">
+    <a class="btn btn-light btn-lg" href = "#" role = "button">Premier League Table</a>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+  </body>
+</html>
---
+++ public_html/navBar.php
@@ -0,0 +1,19 @@
+
+<nav class="navbar sticky-top navbar-expand-lg bg-body-tertiary" style="background-color: #ffffff;">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">
+      <img src="images/ball.png" alt="Logo" width="40" height="40" class="d-inline-block align-text-top" href = "homePage.php">
+      
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse " id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+        <a class="nav-link active" aria-current="page" href="#">Home</a>
+        <a class="nav-link" href="#" >League Table</a>
+        <a class="nav-link" href="#">Fixtures</a>
+        <a class="nav-link" href="#">League Leaders</a>
+      </div>
+  </div>
+</nav>
